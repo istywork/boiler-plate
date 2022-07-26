29a546393bcf27141b868068d4ca27d249ce10b5
istywork <myungs8562@gmail.com>
2022-07-27 01:42:48 +0900

    login logout

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
-
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const { User } = require("./models/User");
 
@@ -20,16 +22,68 @@ app.get("/", (req, res) => {
     res.send("Hello World! 안녕하세요");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
     // 회원가입할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 DB에 넣어준다.
 
     const user = new User(req.body);
+    // save 전에 비밀정보 암호화해야함
     user.save((err, userInfo) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).json({
             success: true,
         });
+    });
+});
+
+app.post("/api/users/login", (req, res) => {
+    //요청된 이메일을 DB에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, userInfo) => {
+        if (!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일이 해당하는 유저가 없습니다.",
+            });
+        }
+        //요청된 이메일이 DB에 있다면 비밀번호가 맞는지 확인
+        userInfo.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 틀렸습니다.",
+                });
+
+            //비밀번호까지 맞다면 토큰을 생성
+            userInfo.generateToken((err, userInfo) => {
+                if (err) return res.status(400).send(err);
+
+                // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지...
+                res.cookie("x_auth", userInfo.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: userInfo._id });
+            });
+        });
+    });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 뜻.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image,
+    });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send({ success: true });
     });
 });
 
