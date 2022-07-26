fe5e5b44502d47f37148ce71887ff8761dcfe596
istywork <myungs8562@gmail.com>
2022-07-26 23:51:30 +0900

    개인정보보호

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const { User } = require("./models/User");
 
 const mongoose = require("mongoose");
 mongoose
-    .connect(
-        "mongodb+srv://seojm:18515@boilerplate.l5hbo.mongodb.net/?retryWrites=true&w=majority"
-    )
+    .connect(config.mongoURI)
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World! 안녕하세요");
+});
+
+app.post("/register", (req, res) => {
+    // 회원가입할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 DB에 넣어준다.
+
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true,
+        });
+    });
 });
 
 app.listen(port, () => {
